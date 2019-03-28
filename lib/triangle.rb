21183c3a5b79a546850e2d9454395cee01aba546
Eric Storey <estorey@gmail.com>
2019-03-28 22:57:39 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,15 @@
 class Triangle
-  # write code here
+  attr :sides
+  
+  def initialize(l1, l2, l3)
+    @sides=[l1, l2, l3]
+    @sides.sort!
+  end
+  
+  def kind 
+  end
+  
+  class TriangleError < StandardError
+  end
+  
 end
